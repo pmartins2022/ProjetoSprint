44ebea33c6465fa83a5f304678f0b6760edb2afa
Diogo Gonçalves <diogo.goncalves-t16@upskill.pt>
2022-06-03 15:37:53 +0100

    Testes as classes controller e service de ProjetoWS

--- Projetos/ProjetoWS/src/test/java/com/grupo2/projeto/service/ProjetoServiceUnitTest.java
+++ Projetos/ProjetoWS/src/test/java/com/grupo2/projeto/service/ProjetoServiceUnitTest.java
@@ -1,14 +1,28 @@
 package com.grupo2.projeto.service;
 
+import com.grupo2.projeto.dto.ProjetoDTO;
 import com.grupo2.projeto.dto.mapper.ProjetoDTOMapper;
+import com.grupo2.projeto.exception.ErroGeralException;
+import com.grupo2.projeto.model.Projeto;
 import com.grupo2.projeto.repository.ProjetoRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.mockito.Mockito.mock;
+import javax.transaction.Transactional;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Transactional
 class ProjetoServiceUnitTest
 {
     @MockBean
@@ -26,7 +40,56 @@ class ProjetoServiceUnitTest
         MockitoAnnotations.openMocks(this);
     }
 
+    @Test
+    public void shouldCreateValidProjeto()
+    {
+        ProjetoDTO projetoDTOMock = mock(ProjetoDTO.class);
+        Projeto projetoMock = mock(Projeto.class);
 
+        when(mapper.toModel(projetoDTOMock)).thenReturn(projetoMock);
+        when(repository.saveProjeto(projetoMock)).thenReturn(projetoMock);
+        when(mapper.toDTO(projetoMock)).thenReturn(projetoDTOMock);
 
+        ProjetoDTO saved = service.createAndSave(projetoDTOMock);
+
+        assertEquals(projetoDTOMock, saved);
+    }
+
+    @Test
+    public void shouldNotCreateProjeto_Exist()
+    {
+        ProjetoDTO projetoDTOMock = mock(ProjetoDTO.class);
+        Projeto projetoMock = mock(Projeto.class);
+
+        when(mapper.toModel(projetoDTOMock)).thenReturn(projetoMock);
+
+        when(repository.saveProjeto(projetoMock)).thenThrow(ErroGeralException.class);
+
+        assertThrows(ErroGeralException.class,()->service.createAndSave(projetoDTOMock));
+    }
+
+    @Test
+    public void shouldFindProjeto_Exists()
+    {
+        ProjetoDTO projetoDTOMock = mock(ProjetoDTO.class);
+        Projeto projetoMock = mock(Projeto.class);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(projetoMock));
+        when(mapper.toDTO(projetoMock)).thenReturn(projetoDTOMock);
+
+        Optional<ProjetoDTO> saved = service.findById(1L);
+
+        assertEquals(projetoDTOMock, saved.get());
+    }
+
+    @Test
+    public void shouldNotFindAnoLetivo_NotExists()
+    {
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<ProjetoDTO> saved = service.findById(1L);
+
+        assertEquals(Optional.empty(), saved);
+    }
 
 }
