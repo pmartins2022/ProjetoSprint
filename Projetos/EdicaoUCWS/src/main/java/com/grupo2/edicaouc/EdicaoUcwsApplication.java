b911e4c6ad8bab02bf0e80455d605f2858ae0b22
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-06-14 18:39:08 +0100

    PropostaWS melhorando a eficácia do código

--- Projetos/EdicaoUCWS/src/main/java/com/grupo2/edicaouc/EdicaoUcwsApplication.java
+++ Projetos/EdicaoUCWS/src/main/java/com/grupo2/edicaouc/EdicaoUcwsApplication.java
@@ -1,10 +1,7 @@
 package com.grupo2.edicaouc;
 
-import com.grupo2.edicaouc.service.EdicaoUCService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EdicaoUcwsApplication {
