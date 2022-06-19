7a8e7a6a11f257a2f5e2e0439abea44838f8ad42
Diogo Gonçalves <diogo.goncalves-t16@upskill.pt>
2022-06-19 16:49:31 +0100

    Unit test de UtilizadoresWS

--- Projetos/UtilizadoresWS/src/test/java/com/grupo2/utilizadores/controller/UtilizadorControllerUnitTest.java
+++ Projetos/UtilizadoresWS/src/test/java/com/grupo2/utilizadores/controller/UtilizadorControllerUnitTest.java
@@ -1,6 +1,8 @@
 package com.grupo2.utilizadores.controller;
 
+import com.grupo2.utilizadores.dto.UtilizadorAuthDTO;
 import com.grupo2.utilizadores.dto.UtilizadorDTO;
+import com.grupo2.utilizadores.exception.ListaVaziaException;
 import com.grupo2.utilizadores.exception.OptionalVazioException;
 import com.grupo2.utilizadores.exception.ValidacaoInvalidaException;
 import com.grupo2.utilizadores.service.UtilizadorService;
@@ -54,5 +56,85 @@ class UtilizadorControllerUnitTest {
         assertThrows(OptionalVazioException.class,()->controller.findByID(1L));
     }
 
+    @Test
+    public void shouldRegistarUtilizador() {
+        UtilizadorDTO utilizadorDTOMOCK = mock(UtilizadorDTO.class);
 
+        when(service.registar(utilizadorDTOMOCK)).thenReturn(utilizadorDTOMOCK);
+
+        ResponseEntity<?> responseEntity = controller.registar(utilizadorDTOMOCK);
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }
+
+   /* @Test
+    public void shouldNotRegistarUtilizador() {
+        UtilizadorDTO utilizadorDTOMOCK = mock(UtilizadorDTO.class);
+
+        when(service.registar(utilizadorDTOMOCK)).thenReturn(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class,()->controller.registar(utilizadorDTOMOCK));
+    }*/
+
+    @Test
+    public void shouldFindByUsername() {
+        UtilizadorAuthDTO utilizadorDTOMOCK = mock(UtilizadorAuthDTO.class);
+
+        when(service.findByUsername("nome")).thenReturn(Optional.of(utilizadorDTOMOCK));
+
+        ResponseEntity<UtilizadorAuthDTO> responseEntity = controller.findByUsername("nome");
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldNotFindByUsername() {
+
+        when(service.findByUsername("nome")).thenReturn(Optional.empty());
+
+        assertThrows(OptionalVazioException.class,()->controller.findByUsername("nome"));
+    }
+
+    @Test
+    public void shouldFindAll()
+    {
+        UtilizadorDTO utilizadorDTOMOCK = mock(UtilizadorDTO.class);
+        List<UtilizadorDTO> list = List.of(utilizadorDTOMOCK, utilizadorDTOMOCK, utilizadorDTOMOCK);
+
+        when(service.findAll()).thenReturn(list);
+
+        ResponseEntity<Object> responseEntity = controller.listAll();
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldNotFindAllByUCCode_NotExists()
+    {
+        when(service.findAll()).thenReturn(List.of());
+
+        assertThrows(ListaVaziaException.class, () -> controller.listAll());
+    }
+
+   /* @Test
+    public void shouldIsRole()
+    {
+        UtilizadorDTO utilizadorDTOMOCK = mock(UtilizadorDTO.class);
+
+        when(service.isRole("papel", 1L)).thenReturn(utilizadorDTOMOCK.getTipoUtilizador(), utilizadorDTOMOCK.getId());
+
+        ResponseEntity<Boolean> responseEntity = controller.isRole("papel", 1L);
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }*/
+
+   /* @Test
+    public void shouldNotIsRole()
+    {
+        UtilizadorDTO utilizadorDTOMOCK = mock(UtilizadorDTO.class);
+
+        when(service.isRole("papel", 1L)).thenReturn(OptionalVazioException.class);
+
+        assertThrows(OptionalVazioException.class,()->controller.isRole(utilizadorDTOMOCK));
+    }*/
 }
