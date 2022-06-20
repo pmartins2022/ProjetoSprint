c3efd020491146d20dabdfffa2a8da6d0caf3dae
Diogo Gonçalves <diogo.goncalves-t16@upskill.pt>
2022-06-20 15:45:50 +0100

    Integration test controller PropostaWS

--- Projetos/PropostaWS/src/test/java/com/grupo2/proposta/service/PropostaServiceIntegrationTests.java
+++ Projetos/PropostaWS/src/test/java/com/grupo2/proposta/service/PropostaServiceIntegrationTests.java
@@ -1,29 +1,49 @@
 package com.grupo2.proposta.service;
 
-import com.grupo2.proposta.dto.PropostaDTO;
+import com.grupo2.proposta.dto.*;
 import com.grupo2.proposta.dto.mapper.PropostaDTOMapper;
 import com.grupo2.proposta.exception.AtualizacaoInvalidaException;
 import com.grupo2.proposta.exception.IdInvalidoException;
+import com.grupo2.proposta.exception.OptionalVazioException;
 import com.grupo2.proposta.jpa.PropostaJPA;
-import com.grupo2.proposta.model.PropostaEstado;
+import com.grupo2.proposta.model.*;
 import com.grupo2.proposta.model.factory.PropostaFactory;
+import com.grupo2.proposta.repository.ConviteRepository;
+import com.grupo2.proposta.repository.PropostaRepository;
 import com.grupo2.proposta.repository.jpa.PropostaJPARepository;
+import com.grupo2.proposta.repository.rest.EdicaoUCRestRepository;
+import com.grupo2.proposta.repository.rest.OrganizacaoRestRepository;
+import com.grupo2.proposta.repository.rest.UtilizadorRestRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
 class PropostaServiceIntegrationTests
 {
-    @Autowired
-    private PropostaService service;
+    @MockBean
+    private EdicaoUCRestRepository edicaoUCRestRepository;
+
+    @MockBean
+    private UtilizadorRestRepository utilizadorRestRepository;
+
+    @MockBean
+    private OrganizacaoRestRepository organizacaoRestRepository;
+
+    @MockBean
+    private ConviteRepository conviteRepository;
 
     @Autowired
     private PropostaFactory factory;
@@ -32,22 +52,27 @@ class PropostaServiceIntegrationTests
     private PropostaDTOMapper mapper;
 
     @Autowired
+    private PropostaRepository repository;
+
+    @Autowired
     private PropostaJPARepository jpaRepository;
 
+    @Autowired
+    @InjectMocks
+    private PropostaService service;
 
-    @Test
-    public void shouldNotAcceptProposta_InvalidID()
+
+    @BeforeEach
+    public void setup()
     {
-        assertThrows(IdInvalidoException.class,()->service.acceptCandidaturaProposta(99L,1L));
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void shouldNotAcceptProposta_InvalidUpdate()
+    public void shouldNotAcceptProposta_InvalidRUCID()
     {
-        PropostaJPA p = new PropostaJPA(1L,1L,1L,"AAAAAAAAAA","BBBBBBBBBB","CCCCCCCCCC",1L,PropostaEstado.APROVADO);
-        PropostaJPA save = jpaRepository.save(p);
-
-        assertThrows(AtualizacaoInvalidaException.class,()->service.acceptCandidaturaProposta(save.getId(),2L));
+        when(edicaoUCRestRepository.findByRucID(99L)).thenReturn(List.of());
+        assertThrows(OptionalVazioException.class,()->service.acceptCandidaturaProposta(99L,1L));
     }
 
     @Test
@@ -111,6 +136,63 @@ class PropostaServiceIntegrationTests
     public void shouldNotFindByTitulo_InvalidID()
     {
         List<PropostaDTO> dto = service.findByTitulo("AFASFAAFS");
+
         assertTrue(dto.isEmpty());
+    }
+
+    @Test
+    public void shouldCreateCandidaturaProposta()
+    {
+        Proposta proposta = new Proposta(1L, 1L, 1L, "tituloMuitaGiro",
+                "problemaMuitaGrave", "objetivoQueSeVaiRealizando", 1L, PropostaEstado.CANDIDATURA);
+
+        UtilizadorDTO utilizadorDTO = new UtilizadorDTO(1L, "nome", "sobrenome", "email@email.pt",
+                TipoUtilizador.ALUNO);
+
+        OrganizacaoDTO organizacaoDTO = new OrganizacaoDTO(1L, "denominacao", 123456789L);
+
+        EdicaoUCDTO edicaoUCDTO = new EdicaoUCDTO(1L, "unidadeCurricular", "1990-1991", 1L);
+
+        when(utilizadorRestRepository.findById(proposta.getUtilizadorId())).thenReturn(Optional.of(utilizadorDTO));
+        when(organizacaoRestRepository.findById(proposta.getOrganizacaoId())).thenReturn(Optional.of(organizacaoDTO));
+        when(edicaoUCRestRepository.findById(proposta.getEdicaoUCId())).thenReturn(Optional.of(edicaoUCDTO));
+
+        Proposta save = repository.save(proposta);
+
+        assertEquals(proposta, save);
+    }
+
+    @Test
+    public void shouldFindByNif()
+    {
+
+        OrganizacaoDTO organizacaoDTO = new OrganizacaoDTO(1L, "denominacao", 123456789L);
+
+        Proposta proposta = new Proposta(1L, 1L, 1L, "tituloMuitaGiro",
+            "problemaMuitaGrave", "objetivoQueSeVaiRealizando", 1L, PropostaEstado.CANDIDATURA);
+
+        Proposta save = repository.save(proposta);
+
+        when(organizacaoRestRepository.findByNIF(123456789, "encode")).thenReturn(Optional.of(organizacaoDTO));
+
+        List<Proposta> list = repository.findByNif(organizacaoDTO);
+
+        assertEquals(list.size(), 1);
+    }
+
+    @Test
+    public void shouldAcceptOrientacaoProposta()
+    {
+        UtilizadorDTO utilizadorDTO = new UtilizadorDTO(1L, "nome", "sobrenome", "email@email.pt",
+                TipoUtilizador.DOCENTE);
+        Proposta proposta = new Proposta(1L, 1L, 1L, "tituloMuitaGiro",
+                "problemaMuitaGrave", "objetivoQueSeVaiRealizando", 1L, PropostaEstado.CANDIDATURA);
+
+        Convite convite = new Convite(1L, 1L, 1L, EstadoConvite.ACEITE);
+
+        when(utilizadorRestRepository.findById(1L)).thenReturn(Optional.of(utilizadorDTO));
+        when(conviteRepository.findByDocenteAndProposta(1L, 1L)).thenReturn(Optional.of(convite));
+
+        assertEquals(convite.getEstado(), EstadoConvite.ACEITE);
     }
 }
